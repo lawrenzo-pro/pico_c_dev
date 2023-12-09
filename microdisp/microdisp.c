ec2d96ddb35ed4385111bf2ce10b541476ce0d7a
Lawrence Kimani <you@example.com>
2023-12-09 14:38:54 +0300

    Microdisp cleaner

--- microdisp/microdisp.c
+++ microdisp/microdisp.c
@@ -2,41 +2,28 @@
 #include "pico/multicore.h"
 #include <stdio.h>
 
-int pins[] = {12,13,14,15,16,17,18,19};
-
-
+int pins [] = {12,13,14,15,16,17,18,19};
+int zero [] = {0,1,2,4,5,6}; // len = 6
+int one  [] = {0,6}; // len = 2
+int two  [] = {0,1,3,4,5}; //len = 5
+int three[] = {0,1,3,5,6}; // len = 5
+int four[]  = {0,2,3,6};   // len = 4
+int five[]  = {1,2,3,5,6}; // len = 5
+int six[]   = {1,2,3,4,5,6}; // len = 6
+int seven[] = {0,1,6};       // len = 3
+int eight[] = {0,1,2,3,4,5,6}; // len = 7
+int nine[]  = {0,1,2,3,5,6}; // len = 6
 //magic
 void setup(){
+    //a simple cheat
     int i = 0;
-    /*
-    //the good way but it doesn't work for some reason
-    int i = 0;
-    while(i < sizeof(pins) - 1){
+    while(i < 8){
        gpio_init(pins[i]);
        gpio_set_dir(pins[i],GPIO_OUT);
        printf("%d  ,", pins[i]);
        i++;
     }
     printf("have been initialized. \n");
-     */
-    //so we have this, tedious mess of code
-    gpio_init(pins[0]);
-    gpio_init(pins[1]);
-    gpio_init(pins[2]);
-    gpio_init(pins[3]);
-    gpio_init(pins[4]);
-    gpio_init(pins[5]);
-    gpio_init(pins[6]);
-    gpio_init(pins[7]);
-    gpio_set_dir(pins[0],GPIO_OUT);
-    gpio_set_dir(pins[1],GPIO_OUT);
-    gpio_set_dir(pins[2],GPIO_OUT);
-    gpio_set_dir(pins[3],GPIO_OUT);
-    gpio_set_dir(pins[4],GPIO_OUT);
-    gpio_set_dir(pins[5],GPIO_OUT);
-    gpio_set_dir(pins[6],GPIO_OUT);
-    gpio_set_dir(pins[7],GPIO_OUT);
-
 }
 void beepy(){
     while(true){
@@ -48,117 +35,94 @@ void beepy(){
 }
 //clear the display
 void clear(){
-    gpio_put(pins[0],0);
-    gpio_put(pins[1],0);
-    gpio_put(pins[2],0);
-    gpio_put(pins[3],0);
-    gpio_put(pins[4],0);
-    gpio_put(pins[5],0);
-    gpio_put(pins[6],0);
+   for(int i = 0; i < 7;i++){
+    gpio_put(pins[i],0);
+   }
 }
 //computers are stupid, I have to define the 9 digits,
 //I'm still looking for a better way of doing this;
 //TODO: See if making the functions return int makes it bettter
-void zero(){
-    gpio_put(pins[0],1);
-    gpio_put(pins[1],1);
-    gpio_put(pins[2],1);
-    gpio_put(pins[4],1);
-    gpio_put(pins[5],1);
-    gpio_put(pins[6],1);
+void print_zero(){
+    for(int i = 0; i<6; i++){
+        gpio_put(pins[zero[i]],1);
+    }
 }
-void one(){
-    gpio_put(pins[0],1);
-    gpio_put(pins[6],1);
+void print_one(){
+   for(int i = 0; i < 2; i++){
+    gpio_put(pins[one[i]],1);
+   }
 }
-void two(){
-    gpio_put(pins[0],1);
-    gpio_put(pins[1],1);
-    gpio_put(pins[3],1);
-    gpio_put(pins[4],1);
-    gpio_put(pins[5],1);
+void print_two(){
+    for(int i = 0; i < 5; i++){
+        gpio_put(pins[two[i]],1);
+    }
 }
-void three(){
-    gpio_put(pins[0],1);
-    gpio_put(pins[1],1);
-    gpio_put(pins[3],1);
-    gpio_put(pins[5],1);
-    gpio_put(pins[6],1);
+void print_three(){
+   for(int i = 0; i < 5; i++){
+    gpio_put(pins[three[i]],1);
+   }
 }
-void four(){
-    gpio_put(pins[0],1);
-    gpio_put(pins[2], 1);
-    gpio_put(pins[3],1);
-    gpio_put(pins[6],1);
+void print_four(){
+    for(int i = 0; i < 4; i++){
+        gpio_put(pins[four[i]],1);
+    }
 }
-void five(){
-    gpio_put(pins[1],1);
-    gpio_put(pins[2],1);
-    gpio_put(pins[3],1);
-    gpio_put(pins[5],1);
-    gpio_put(pins[6],1);
+void print_five(){
+   for(int i = 0; i< 5; i++){
+    gpio_put(pins[five[i]],1);
+   }
 }
-void six(){
-    gpio_put(pins[1],1);
-    gpio_put(pins[2],1);
-    gpio_put(pins[3],1);
-    gpio_put(pins[4],1);
-    gpio_put(pins[5],1);
-    gpio_put(pins[6],1);
+void print_six(){
+  for(int i = 0; i < 6;i++){
+    gpio_put(pins[six[i]], 1);
+  }
 }
-void seven(){
-    gpio_put(pins[0],1);
-    gpio_put(pins[1],1);
-    gpio_put(pins[6],1);
+void print_seven(){
+    for(int i = 0; i< 3;i++){
+        gpio_put(pins[seven[i]],1);
+    }
 }
-void eight(){
+void print_eight(){
     //same here but this implementation is cursed either way
-    gpio_put(pins[0],1);
-    gpio_put(pins[1],1);
-    gpio_put(pins[2],1);
-    gpio_put(pins[3],1);
-    gpio_put(pins[4],1);
-    gpio_put(pins[5],1);
-    gpio_put(pins[6],1);
+    for(int i = 0; i < 7;i++){
+        gpio_put(pins[eight[i]],1);
+    }
 }
-void nine(){
-    gpio_put(pins[0],1);
-    gpio_put(pins[1],1);
-    gpio_put(pins[2],1);
-    gpio_put(pins[3],1);
-    gpio_put(pins[5],1);
-    gpio_put(pins[6],1);
+void print_nine(){
+   for(int i = 0; i < 6;i++){
+    gpio_put(pins[nine[i]],1);
+   }
 }
 void countdown(){
     while(true){
-        zero();
+        print_zero();
         sleep_ms(1000);
         clear();
-        one();
+        print_one();
         sleep_ms(1000);
         clear();
-        two();
+        print_two();
         sleep_ms(1000);
         clear();
-        three();
+        print_three();
         sleep_ms(1000);
         clear();
-        four();
+        print_four();
         sleep_ms(1000);
         clear();
-        five();
+        print_five();
         sleep_ms(1000);
         clear();
-        six();
+        print_six();
         sleep_ms(1000);
         clear();
-        seven();
+        print_seven();
         sleep_ms(1000);
         clear();
-        eight();
+        print_eight();
         sleep_ms(1000);
         clear();
-        nine();
+        print_nine();
         sleep_ms(1000);
         clear();
     }
